87fc9993039c3018e8ceff7fad9961cc8cd6a66d
AdRusinek <rusinekadrian19@gmail.com>
2019-02-15 22:43:11 +0100

    done handling Number Format exception

--- src/main/java/com/rusinek/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/rusinek/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.rusinek.recipeapp.controllers;
 
 import com.rusinek.recipeapp.commands.RecipeCommand;
+import com.rusinek.recipeapp.exceptions.NotFoundException;
 import com.rusinek.recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -55,5 +58,33 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception",exception);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleBadFormat(Exception exception) {
+
+        log.error("Handling Number format exc");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception",exception);
+
+        return modelAndView;
     }
 }
